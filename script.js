0f2b754b73ad7950d25e1f1927215e1dfa31e27c
= <mattmims@DESKTOP-9933GDS.localdomain>
2018-10-20 16:23:01 -0400

    Updated js

--- script.js
+++ script.js
@@ -15,3 +15,37 @@ function toggleEmail() {
         emailform.style.display = "block";
     }
 }
+
+function trySubmit() {
+    const textFormat = /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/;
+    const emailFormat = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    var canSubmit = true;
+    var usernameExists = document.getElementById("username") == "";
+    var passwordExists = document.getElementById("password") == "";
+    if (!usernameExists) {
+        console.log("Please enter a username.");
+        canSubmit = false;
+    } if (!passwordExists) {
+        console.log("Please enter a password.");
+        canSubmit = false;
+    } if (!(document.getElementById("textradio").checked || document.getElementById("emailradio").checked)) {
+        console.log("Please select a contact method.");
+        canSubmit = false;
+    } if (document.getElementById("textradio").checked) {
+        if (!textFormat.test(document.getElementById("phonenumber"))) {
+            console.log("Please enter a valid phone number.");
+            canSubmit = false;
+        }
+    } if (document.getElementById("emailradio").checked) {
+        if (!textFormat.test(document.getElementById("email"))) {
+            console.log("Please enter a valid email address.");
+            canSubmit = false;
+        }
+    }
+    if (usernameExists && passwordExists) {
+        //TODO: check for match
+    }
+    if (canSubmit) {
+        submit(); //TODO: define submit function
+    }
+}
